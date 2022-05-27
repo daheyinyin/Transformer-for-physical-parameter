d3d14ca0a7f67c120ae7c0550c197da1f415c346
“liaozy” <atliaozhiying@outlook.com>
2022-05-28 00:33:29 +0800

    fixbug

---
+++ config.py
@@ -0,0 +1,17 @@
+from addict import Dict
+
+config = Dict(n_layers = 6,
+              num_heads = 8,
+              embed_dim_src = 2000,
+              embed_dim_tar = 150,
+              max_seq_len_src = 3,
+              max_seq_len_tar = 4+1,
+              positive_index = [1-1, 3-1],
+              dropout=0.1,
+              data_dict = dict(source_mean=[200.0, 166.675, 1086.1372078947368],source_std=[1,128.41554355980094, 2064.9014864208366],
+                 target_mean=[56.605398270569715, 36.603037880718155, 156.93791664264134, -133.1116516062647],
+                 target_std=[141.5582450571235, 13.412551904219523, 282.0334557891527, 7.86867042633041]),
+              batch_size = 32,
+              ckpt = None,
+)
+
